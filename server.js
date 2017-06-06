6cb998f7accb91c63602e2e343437149536c7fe1
M R <M R>
2017-06-06 17:10:52 -0400

    heroku progress

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-// Dependencies
+// Dependencies required
 var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var path = require("path");
+
 // Requiring our Note and Article models
 var Note = require("./models/note.js");
 var Article = require("./models/article.js");
 var User = require("./models/User.js");
+
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
+
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
 
@@ -18,7 +21,7 @@ mongoose.Promise = Promise;
 // Initialize Express
 var app = express();
 
-// Use morgan and body parser with our app
+// Use morgan and body parser with our current app
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
   extended: false
